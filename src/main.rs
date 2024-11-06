bf56150cc0aa259b57cbf247c7982966fc01e976
Ernesto Ramírez <ernestorb_@outlook.com>
2024-11-05 23:44:37 -0600

    Ejecutar código desde entrada estándar

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Args, Parser as CParser, Subcommand};
 use std::fs;
+use std::io::Read;
 use vainilla_machine::parse;
 use vainilla_machine::vm;
 
@@ -13,14 +14,13 @@ struct Cli {
     /// Turn on/off processing output
     debug: bool,
     // #[arg(short, long)]
-    // /// Dont write tokens to files
-    // no_save: bool,
 }
 
 #[derive(Subcommand, Clone)]
 enum Commands {
     /// Scan files and put output on the same dir with the same name of the file but .lex appended
     Run(RunArgs),
+    RunStdin,
     /// Scan files and put output on the same dir with the same name of the file but .lex appended
     Parse(RunArgs),
 }
@@ -106,6 +106,62 @@ fn main() {
             let instructions = parser.parse_file(&contents);
             for instr in instructions {
                 println!("{:?}", instr);
+            }
+        }
+        Commands::RunStdin => {
+            let mut contents = String::new();
+            std::io::stdin()
+                .read_to_string(&mut contents)
+                .expect("Something went wrong reading from stdin");
+
+            let mut parser = parse::Parser::new();
+            let instructions = parser.parse_file(&contents);
+            let mut vm = vm::VM::new(instructions);
+
+            if cli.debug {
+                println!("Ejecutando programa en modo depuración...");
+                loop {
+                    println!("Seleccione una opción:");
+                    println!("1. Ejecutar una instrucción");
+                    println!("2. Ejecutar todo el programa");
+                    println!("3. Ver contenido de la pila");
+                    println!("4. Ver contenido de las variables");
+                    println!("5. Salir");
+
+                    let mut choice = String::new();
+                    std::io::stdin()
+                        .read_line(&mut choice)
+                        .expect("Failed to read line");
+                    let choice = choice.trim();
+
+                    match choice {
+                        "1" => {
+                            println!("Instruccion actual: {:?}", vm.current_instruction());
+                            vm.step();
+                            println!("Instruccion siguiente: {:?}", vm.current_instruction());
+                        }
+                        "2" => {
+                            vm.run();
+                        }
+                        "3" => {
+                            println!("Contenido de la pila:");
+                            vm.print_stack();
+                        }
+                        "4" => {
+                            println!("Contenido de las variables:");
+                            vm.print_vars();
+                        }
+                        "5" => {
+                            break;
+                        }
+                        _ => {
+                            println!("Opción no válida, por favor intente de nuevo.");
+                        }
+                    }
+                }
+            } else {
+                println!("Ejecutando programa...");
+                vm.run();
             }
         }
     }
